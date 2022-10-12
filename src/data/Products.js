39ac2c62f8e9772bec8ca216e406b36469f87326
zahra mortazavi <zahramortazavi23@gmail.com>
2022-10-12 11:45:09 -0700

    repository created

---
+++ src/data/Products.js
@@ -0,0 +1,102 @@
+const products =[
+    {
+        _id:"1",
+        name:"Sandal",
+        image:"/images/shoe1.png",
+        decription:"Sandals are a type of footwear, generally an open type.",
+        price:89,
+        countInStock:3,
+        rating:4,
+        numReviws:5
+    },
+    {
+        _id:"2",
+        name:"/images/shoe2.png",
+        image:"Ladies Sandal",
+        decription:"Sandals are a type of footwear, generally an open type.",
+        price:214,
+        countInStock:5,
+        rating:3,
+        numReviws:2
+    },
+    {
+        _id:"3",
+        name:"Satin Sandal",
+        image:"/images/shoe3.png",
+        decription:"Sandals are a type of footwear, generally an open type.",
+        price:599,
+        countInStock:0,
+        rating:3.5,
+        numReviws:3
+    },
+    {
+        _id:"4",
+        name:"Satin Sandal",
+        image:"/images/shoe4.png",
+        decription:"Sandals are a type of footwear, generally an open type.",
+        price:123,
+        countInStock:10,
+        rating:5,
+        numReviws:9
+    },
+    {
+        _id:"5",
+        name:"Satin Sandal",
+        image:"/images/shoe5.png",
+        decription:"Sandals are a type of footwear, generally an open type.",
+        price:423,
+        countInStock:6,
+        rating:4,
+        numReviws:3
+    },
+    {
+        _id:"6",
+        name:"Ladies Sandal",
+        image:"/images/shoe6.png",
+        decription:"Sandals are a type of footwear, generally an open type.",
+        price:198,
+        countInStock:5,
+        rating:2,
+        numReviws:3
+    },
+    {
+        _id:"7",
+        name:"Satin Sandal",
+        image:"/images/shoe7.png",
+        decription:"Sandals are a type of footwear, generally an open type.",
+        price:145,
+        countInStock:9,
+        rating:3,
+        numReviws:4
+    },
+    {
+        _id:"8",
+        name:"Ladies Sandal",
+        image:"/images/shoe8.png",
+        decription:"Sandals are a type of footwear, generally an open type.",
+        price:213,
+        countInStock:7,
+        rating:5,
+        numReviws:4
+    },
+    {
+        _id:"9",
+        name:"Satin Sandal",
+        image:"/images/shoe9.png",
+        decription:"Sandals are a type of footwear, generally an open type.",
+        price:243,
+        countInStock:10,
+        rating:2,
+        numReviws:2
+    },
+    {
+        _id:"10",
+        name:"Ladies Sandal",
+        image:"/images/shoe10.png",
+        decription:"Sandals are a type of footwear, generally an open type.",
+        price:352,
+        countInStock:9,
+        rating:4,
+        numReviws:2
+    }
+]
