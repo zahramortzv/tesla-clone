e863a1d6855c2c78e89fc8addec5b5439395a29a
zahra mortazavi <zahramortazavi23@gmail.com>
2022-10-13 13:36:18 -0700

    Menu create

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,75 @@
 import React from 'react';
+import styled from 'styled-components';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <a>
+                <img src="/icons/logo.svg"/>
+            </a>
+
+            <Menu>
+                <a href='#'>Model S</a> 
+                <a href='#'>Model 3</a> 
+                <a href='#'>Model X</a> 
+                <a href='#'>Model Y</a> 
+            </Menu>
+            <RightMenu>
+                <a href='#'>Shop</a>
+                <a href='#'>Tesla Account</a>
+                <CustomMenu>
+                </CustomMenu>
+            </RightMenu>
+        </Container>
     );
 };
 
 export default Header;
+
+const Container = styled.div`
+    min-heigh: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    top: 0;
+    left: 0;
+    right: 0;
+`
+
+const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    flex: 1;
+    justify-content: center;
+
+    a{
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 0 10px;
+        flex-wrap: no-wrap
+    }
+
+    @media(max-width: 768px){
+        display: none;
+    }
+
+`
+
+const RightMenu = styled.div`
+    display: flex;
+    align-items: center;
+
+    a{
+        font-weight: 600;
+        text-transform: uppercase;
+        margin-right: 10px;
+        flex-wrap: no-wrap
+    }
+`
+
+const CustomMenu = styled(MenuIcon)`
+
+`
